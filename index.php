46ac2976a4b51acfc89f0fb3f89696ab3e3a4381
vrispo <veronica_rispo@hotmail.it>
2021-10-04 16:56:55 +0200

    aggiornamento varie sezioni

--- index.php
+++ index.php
@@ -71,7 +71,33 @@
                     </div>                   
                 </form>              
             </div>
-            
+            <br>
+            <div class='info-container'>
+                <div class='info-box'>
+                    <div class="info-img" style="background-image: url('./Images/Get Best Prices.png')"></div>
+                    <div class="info-title"><p>Get Best Prices</p></div>
+                    <div class="info-text"><p>Pay through our application and save thousands and get amazing rewards</p></div>
+                </div>
+                <div class='info-box'>
+                    <div class="info-img" style="background-image: url('./Images/Covid Safe.png')"></div>
+                    <div class="info-title"><p>Covid Safe</p></div>
+                    <div class="info-text"><p>We have all the curated hotels that have all the precaution for a covid safe environment</p></div>
+                </div>
+                <div class='info-box'>
+                    <div class="info-img" style="background-image: url('./Images/Flexible Payment.png')"></div>
+                    <div class="info-title"><p>Flexible Payment</p></div>
+                    <div class="info-text"><p>Enjoy the flexible payment through our app and get rewards on every payment</p></div>                    
+                </div>
+                <div class='info-box'>
+                    <div class="info-img" style="background-image: url('./Images/Nearby.png')"></div>
+                    <div class="info-title"><p>Find The Best Near You</p></div>
+                    <div class="info-text"><p>Find the best hotels and places to visit near you in a single click</p></div>                    
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="destination-container">
+            <div class="destination-title">Recommended Destination</div>
         </div>
                
         <div id ="pie"> 
